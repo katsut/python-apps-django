9a0a937b640b939eae814a34f3cc2bc84454aa8b
Katsuhide Tsuruta <katsuhide.tsuruta@gmail.com>
2025-10-23 16:35:55 +0900

    deploy settings

--- python_apps_django/settings.py
+++ python_apps_django/settings.py
@@ -141,8 +141,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = "static/"
-STATIC_ROOT = BASE_DIR / "staticfiles"  # deployment setting for Railway
-STATICFILES_DIRS = [BASE_DIR / "static"]  # deployment setting for Railway
+# STATIC_ROOT = BASE_DIR / "staticfiles"  # deployment setting for Railway
+# STATICFILES_DIRS = [BASE_DIR / "static"]  # deployment setting for Railway
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
