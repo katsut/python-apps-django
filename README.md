c50eee497ddf8bb1e579123b73d93d76e2eeb5c4
Katsuhide Tsuruta <katsuhide.tsuruta@gmail.com>
2025-10-23 22:53:20 +0900

    improve design

--- README.md
+++ README.md
@@ -13,4 +13,16 @@ pip install gunicorn whitenoise psycopg
 pip freeze > requirements.txt
 ```
 
+## スーパーユーザーを登録
 
+新しく作る
+
+```sh
+python manage.py createsuperuser
+```
+
+パスワードを変える
+
+```sh
+python manage.py changepassword admin
+```
--- python_apps_django/urls.py
+++ python_apps_django/urls.py
@@ -50,4 +50,19 @@ urlpatterns = [
         ),
         name="signup",
     ),
+    path(
+        "password_change/",
+        auth_views.PasswordChangeView.as_view(
+            template_name="registration/password_change_form.html",
+            success_url=reverse_lazy("password_change_done"),
+        ),
+        name="password_change",
+    ),
+    path(
+        "password_change/done/",
+        auth_views.PasswordChangeDoneView.as_view(
+            template_name="registration/password_change_done.html"
+        ),
+        name="password_change_done",
+    ),
 ]
